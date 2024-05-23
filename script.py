8a2f32a48c2b3865189b0e91291dbec6b3a394af
Raphael Vitorio <raphaelvitorio01@gmail.com>
2024-05-23 03:35:37 -0300

    changes

--- script.py
+++ script.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DATABASE_USER")
+DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DB_HOST = os.getenv("DATABASE_HOST")
+DB_PORT = os.getenv("DATABASE_PORT")
+DB_NAME = os.getenv("DATABASE_NAME")
 
 def extract_data_from_pdf(pdf_path):
     global id
