772425942f6c01cecf0b9c5df3a6a7898e3eb922
Raphael Vitorio <raphaelvitorio01@gmail.com>
2024-05-24 20:08:35 -0300

    static server implement fixes

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use("/faturas", faturaRoutes)
 
 app.listen(PORT, () => {
